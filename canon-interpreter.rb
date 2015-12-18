58fa2a636dcf2b38ebb48ea6193b1b60baac81e6
Emily Fox <ef337@cam.ac.uk>
2015-12-18 11:47:56 +0000

    Add multiple voices in interpreter.

--- canon-interpreter.rb
+++ canon-interpreter.rb
@@ -1,8 +1,17 @@
-def play_canon(canon)
+def play_melody(canon)
   for i in 0..canon[:num_bars] - 1
     for j in 0..canon[:bars][i][:num_notes] - 1
       play canon[:bars][i][:notes][j][:pitch]
       sleep canon[:bars][i][:notes][j][:duration]
+    end
+  end
+end
+
+def play_canon(canon, voices)
+  for i in 0..voices - 1
+    sleep i * canon[:time_sig][0]
+    in_thread do
+      play_melody(canon)
     end
   end
 end
