7ec77804cf0a38e8fa2d8898d3d7db4653866a86
Emily Fox <ef337@cam.ac.uk>
2015-12-19 09:47:41 +0000

    Change interpreter for new format.

--- canon-interpreter.rb
+++ canon-interpreter.rb
@@ -1,17 +1,35 @@
-def play_melody(canon)
-  for i in 0..canon[:num_bars] - 1
-    for j in 0..canon[:bars][i][:num_notes] - 1
-      play canon[:bars][i][:notes][j][:pitch]
-      sleep canon[:bars][i][:notes][j][:duration]
-    end
+def play_melody(mel)
+  num_bars = mel.length
+  for i in 0..num_bars - 1
+    play_bar(mel[i])
+  end
+end
+
+def play_bar(bar)
+  num_beats = bar.length
+  for i in 0..num_beats - 1
+    play_beat(bar[i])
+  end
+end
+
+def play_beat(beat)
+  pairs = beat[:rhythm].zip(beat[:notes])
+  pairs.map do |pair|
+    play pair[1]
+    sleep pair[0]
   end
 end
 
 def play_canon(canon, voices)
-  for i in 0..voices - 1
-    sleep i * canon[:time_sig][0]
-    in_thread do
-      play_melody(canon)
+  if canon.length == 0 || canon[0].length == 0
+    puts "Warning: empty canon given."
+  else
+    num_beats = canon[0][0].length
+    for i in 0..voices - 1
+      sleep i * num_beats
+      in_thread do
+        play_melody(canon)
+      end
     end
   end
 end
