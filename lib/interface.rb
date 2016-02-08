94340bdc6fa9b7567015703599f4517053c8149f
Emily Fox <ef337@cam.ac.uk>
2016-02-08 15:35:20 +0000

    Update interface. Slow and dodgy sounding tunes.

--- lib/interface.rb
+++ lib/interface.rb
@@ -68,7 +68,7 @@ define :canon_play do |canon_arg|
   else
     # Find how many voices to play- this is the same as beats in a bar.
     num_voices = canon.get_metadata.get_number_of_voices
-    num_beats = canon.get_metadata.get_beats_in_bar
+    offset = canon.get_metadata.get_beats_in_bar * canon.get_metadata.get_bars_per_variation
     # Play the melody the correct number of times at the different offsets.
     num_voices.times do
       in_thread do
@@ -76,75 +76,8 @@ define :canon_play do |canon_arg|
         play_melody(canon_internal_rep, rand * 0.75)
       end
       # Sleep until the next bar.
-      sleep num_beats
+      sleep offset
     end
-  end
-end
-
-# ARGS: Metadata object.
-# DESCRIPTION: Generates a complete canon and plays it.
-# RETURNS: Nil.
-define :canon_play_crab do |canon_arg|
-
-  # ARGS: A melody (array of bars which are arrays of beats) and a value for the pan of this melody.
-  # DESCRIPTION: Plays the melody by playing each bar in turn.
-  # RETURNS: Nil.
-  define :play_melody do |melody, pan|
-    num_bars = melody.length
-    for i in 0..num_bars - 1
-      play_bar(melody[i], pan)
-    end
-  end
-
-  # ARGS: A bar (array of beats) and a value for the pan of this melody.
-  # DESCRIPTION: Plays the bar by playing each beat in turn.
-  # RETURNS: Nil.
-  define :play_bar do |bar, pan|
-    num_beats = bar.length
-    for i in 0..num_beats - 1
-      play_beat(bar[i], pan)
-    end
-  end
-
-  # ARGS: A beat (hash with the root note, rhythm values and pitch values) and a value for the pan of this melody.
-  # DESCRIPTION: Plays the beat by playing each note in turn, specified by the array of values for rhythm and pitch.
-  # RETURNS: Nil.
-  define :play_beat do |beat, pan|
-    proportion_sustain = 0.7
-    proportion_release = 0.3
-    pairs = beat[:rhythm].zip(beat[:notes])
-    pairs.map do |pair|
-      play pair[1], attack: 0, sustain: pair[0] * proportion_sustain, release: pair[0] * proportion_release, pan: pan
-      sleep pair[0].to_f
-    end
-  end
-
-  # If the argument is metadata, generate a canon from it.
-  if canon_arg.is_a?(Metadata)
-    canon = Canon.new(canon_arg)
-  elsif canon_arg.is_a?(Canon)
-    canon = canon_arg
-  else
-    raise "Wrong argument type for canon_play: #{ metadata.class }."
-  end
-  # Get the array representation of the canon from the canon object.
-  canon_internal_rep = canon.get_canon_as_array
-  # Check for an empty canon.
-  if canon_internal_rep.length == 0 || canon_internal_rep[0].length == 0
-    raise "This canon is empty"
-  else
-    canon_reversed = canon_internal_rep.reverse
-    for bar in 0..canon_reversed.length - 1
-      canon_reversed[bar] = canon_reversed[bar].reverse
-      for beat in 0..canon_reversed[bar].length - 1
-        canon_reversed[bar][beat][:notes] = canon_reversed[bar][beat][:notes].reverse
-        canon_reversed[bar][beat][:rhythm] = canon_reversed[bar][beat][:rhythm].reverse
-      end
-    end
-    in_thread do
-      play_melody(canon_internal_rep, rand * 0.75)
-    end
-    play_melody(canon_reversed, rand * 0.75)
   end
 end
 
